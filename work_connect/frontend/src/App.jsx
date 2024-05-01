0d1667c2beb212e1b0cb96bed24688dcbe38143a
R202-224\lightbox2 <2211060@i-seifu.jp>
2024-05-01 17:00:06 +0900

    変更を加えました

--- work_connect/frontend/src/App.jsx
+++ work_connect/frontend/src/App.jsx
@@ -9,6 +9,8 @@ function App() {
   return (
     <>
     {/* aaaaaaaaaaaaa */}
+    {/* kjafdjkdabfvkbadbebjkenjkbneknb
+     */}
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />

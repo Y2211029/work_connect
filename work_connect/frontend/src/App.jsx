6dd538c7064779a611ed33fcaf99b4ab809f2b0f
Mona Lisa <2211060@i-seifu.jp>
2024-05-22 16:09:43 +0900

    2024/05/22

--- work_connect/frontend/src/App.jsx
+++ work_connect/frontend/src/App.jsx
@@ -1,41 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import ReactDOM from "react-dom/client";
+import "./App.css";
+import "normalize.css"; //こちらを新たに追加//
+import HorizontalLinearStepper from "./components/stepbar.jsx";
+import Header from "./components/Header.jsx";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const App = () => {
   return (
     <>
-    {/* aaaaaaaaaaaaa */}
-    {/* kjafdjkdabfvkbadbebjkenjkbneknb
-    aoaloalalalalaldlalalalalalalalla
-    anklndnlnfnkabfnabab
-    家で追加しましたぜ
-     */}
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Router>
+        <div className="App">
+          <Header />
+          {/* <Routes>
+            <Route path="/" element={<BlogPage />} />
+          </Routes> */}
+        </div>
+      </Router>
+      <div className="lowermostPlatform">
+        <header></header>
+        <main>
+          <p>新規登録</p>
+          <div className="test">
+            <HorizontalLinearStepper />
+          </div>
+        </main>
+        <div class="centering_parent">
+          <div class="centering_item"></div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;

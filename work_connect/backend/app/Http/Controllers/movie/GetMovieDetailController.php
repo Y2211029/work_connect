06faa926b3a617fc0274db7ac7d6f2bee51e2c74
ryudayo64 <2211060@i-seifu.jp>
2024-07-22 09:35:45 +0900

    9:35

--- work_connect/backend/app/Http/Controllers/movie/GetMovieDetailController.php
+++ work_connect/backend/app/Http/Controllers/movie/GetMovieDetailController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\movie;
 
 use App\Http\Controllers\Controller;
+use App\Models\w_comment;
 use Illuminate\Http\Request;
 use App\Models\w_movie;
 
@@ -23,13 +24,36 @@ class GetMovieDetailController extends Controller
 
                 )->where('movie_id', $id)->get();
 
+
+            $comments = w_comment::select('w_comments.*')
+                ->leftJoin('w_users', function ($join) {
+                    $join->on('w_comments.commenter_id', '=', 'w_users.id')
+                        ->whereRaw('LEFT(w_comments.commenter_id, 1) = "S"');
+                })
+                ->leftJoin('w_companies', function ($join) {
+                    $join->on('w_comments.commenter_id', '=', 'w_companies.id')
+                        ->whereRaw('LEFT(w_comments.commenter_id, 1) = "C"');
+                })
+                ->select(
+                    'w_comments.*',
+                    'w_users.user_name AS commenter_user_name',
+                    'w_companies.company_name AS commenter_company_name'
+                )
+                ->where('w_comments.various_id', $id)
+                ->where('w_comments.genre', 'works')
+                ->get();
+
+            $movieListArray = [];
+            $movieListArray["動画"] = json_decode(json_encode($movieList), true);
+            $movieListArray["動画コメント"] = json_decode(json_encode($comments), true);
+
             \Log::info('GetMovieDetailController:$id:');
             \Log::info($id);
 
-            \Log::info('GetMovieDetailController:$movieList[0]:');
-            \Log::info(json_decode(json_encode($movieList[0]), true));
-            
-            return json_encode($movieList[0]);
+            \Log::info('GetMovieDetailController:$movieListArray:');
+            \Log::info(json_decode(json_encode($movieListArray), true));
+
+            return json_encode($movieListArray);
         } catch (\Exception $e) {
             \Log::info('GetMovieDetailController:user_name重複チェックエラー');
             \Log::info($e);
--- work_connect/frontend/src/sections/VideoList/VideoDetail-Item.jsx
+++ work_connect/frontend/src/sections/VideoList/VideoDetail-Item.jsx
@@ -6,39 +6,79 @@ import axios from "axios";
 import Box from "@mui/material/Box";
 
 import { useCreateTagbutton } from "src/hooks/use-createTagbutton";
+import { useSessionStorage } from "src/hooks/use-sessionStorage";
 
 const VideoDetailItem = () => {
+  // ログイン情報の取得
+  const { getSessionData } = useSessionStorage();
+  // 動画IDの取得
   const { id } = useParams();
   // -----動画データ-----
   const [VideoDetail, setVideoDetail] = useState([]);
-  
+  const [CommentPost, setCommentPost] = useState({
+    display: "none",
+    text: "",
+  });
+  const [videoComment, setVideoComment] = useState([]);
+  const [AccountData, setAccountData] = useState({});
+  const [Comment, setComment] = useState({});
+  const [CommentCancel, setCommentCancel] = useState("");
+
   // -----タグ-----
   const { tagCreate } = useCreateTagbutton();
   // ジャンル
   const [VideoGenre, setVideoGenre] = useState("");
 
+  // 動画データ
+  const videoDetailUrl = "http://localhost:8000/get_movie_detail";
+  // 動画コメント投稿
+  const videoCommentPostUrl = "http://localhost:8000/post_movie_comment_post";
+  // 動画コメントデータ
+  const videoCommentUrl = "http://localhost:8000/post_movie_comment";
+  // 動画コメント削除
+  const videoCommentDelete = "http://localhost:8000/post_movie_comment_delete";
 
-  const url = "http://localhost:8000/get_movie_detail";
   // Laravel側から動画詳細データを取得
   useEffect(() => {
+    setAccountData(getSessionData("accountData"));
+
     async function VideoListFunction() {
       try {
         // Laravel側から動画詳細データを取得
-        const response = await axios.get(url, {
+        const response = await axios.get(videoDetailUrl, {
           params: { id: id },
           headers: {
             "Content-Type": "json",
           },
         });
 
-        setVideoDetail(response.data);
-        console.log("response.data", response.data);
+        setVideoDetail(response.data["動画"][0]);
+        setVideoComment(response.data["動画コメント"]);
+
+        console.log("response.data[動画][0]", response.data);
+        // console.log("response.data[動画][0]", response.data);
       } catch (err) {
         console.log("err:", err);
       }
     }
     VideoListFunction();
   }, [id]); // 空の依存配列を渡すことで初回のみ実行されるようにする
+
+  useEffect(() => {
+    const initialCommentState = {};
+    videoComment.forEach((value) => {
+      initialCommentState[value.id] = {
+        display: "none",
+        text: value.content,
+        readOnly: true,
+      };
+    });
+    setComment(initialCommentState);
+  }, [videoComment]);
+
+  useEffect(() => {
+    console.log("commentが変更されました。", Comment);
+  }, [Comment]);
 
   // タグ作成
   useEffect(() => {
@@ -70,6 +110,126 @@ const VideoDetailItem = () => {
     </>
   );
 
+  // コメント欄表示
+  const handleTextOpen = () => {
+    setCommentPost({ ...CommentPost, display: "block" });
+  };
+
+  // コメント投稿キャンセル
+  const handlePostCancel = () => {
+    setCommentPost({ ...CommentPost, display: "none", text: "" });
+  };
+
+  // コメント投稿内容
+  const handlePostChange = (value) => {
+    console.log("valuevaluevaluevalue", value);
+    setCommentPost({ ...CommentPost, text: value });
+  };
+
+  const workCommentSave = async (text, movieId, userId) => {
+    try {
+      // Laravel側から作品詳細データを取得
+      await axios.post(videoCommentPostUrl, {
+        movieCommentContent: text,
+        movie_id: movieId,
+        user_id: userId,
+      });
+      // コメント投稿後にページを再読み込み
+      window.location.reload();
+    } catch (err) {
+      console.log("err:", err);
+    }
+  };
+
+  // コメント投稿
+  const handlePost = () => {
+    setCommentPost({ ...CommentPost, display: "none" });
+    if (CommentPost.text.trim() !== "") {
+      workCommentSave(CommentPost.text, id, AccountData.id);
+    } else {
+      alert("空白のまま投稿はできません。");
+    }
+  };
+
+  // コメント編集ボタンクリック
+  const handleClick = (commentId) => {
+    setComment({
+      ...Comment,
+      [commentId]: {
+        ...Comment[commentId],
+        display: "block",
+        readOnly: false,
+      },
+    });
+
+    setCommentCancel(Comment[commentId].text);
+  };
+
+  const handleCancel = (commentId) => {
+    setComment({
+      ...Comment,
+      [commentId]: {
+        ...Comment[commentId],
+        display: "none",
+        text: CommentCancel,
+        readOnly: true,
+      },
+    });
+  };
+
+  // コメント入力内容変更
+  const handleChenge = (text, commentId) => {
+    setComment({
+      ...Comment,
+      [commentId]: {
+        ...Comment[commentId],
+        text: text,
+      },
+    });
+  };
+
+  // コメント保存
+  const handleSave = (commentId) => {
+    setComment({
+      ...Comment,
+      [commentId]: {
+        ...Comment[commentId],
+        display: "none",
+        readOnly: true,
+      },
+    });
+
+    async function workCommentSave() {
+      try {
+        // Laravel側から作品詳細データを取得
+        await axios.post(videoCommentUrl, {
+          workCommentContent: Comment[commentId].text,
+          commentId: commentId,
+        });
+      } catch (err) {
+        console.log("err:", err);
+      }
+    }
+    workCommentSave();
+  };
+
+  // コメント削除
+  const handleDelete = (commentId) => {
+    async function workCommentDeletefunc() {
+      try {
+        // Laravel側から作品詳細データを取得
+        await axios.post(videoCommentDelete, {
+          commentId: commentId,
+        });
+        alert("コメントを削除しました。");
+        // コメント削除にページを再読み込み
+        window.location.reload();
+      } catch (err) {
+        console.log("err:", err);
+      }
+    }
+    workCommentDeletefunc();
+  };
   // 動画URL
   const renderYoutubeFrame = (
     <>
@@ -108,6 +268,54 @@ const VideoDetailItem = () => {
     </>
   );
 
+  const renderComment = videoComment && Object.keys(Comment).length > 0 && (
+    <>
+      {videoComment && Object.keys(Comment).length > 0 && <h3>コメント一覧</h3>}
+      {videoComment.map((item, index) =>
+        (item.commenter_id === AccountData.id && item.commenter_user_name === AccountData.user_name) ||
+        (item.commenter_id === AccountData.id && item.commenter_company_name === AccountData.company_name) ? (
+          <div key={index}>
+            <hr />
+            {/* {console.log("comment", Comment)} */}
+            <button onClick={() => handleClick(item.id)}>編集</button>
+            <button onClick={() => handleCancel(item.id)} className={`comment_${item.id}`} style={{ display: Comment[item.id]?.display }}>
+              キャンセル
+            </button>
+            <button onClick={() => handleSave(item.id)} className={`comment_${item.id}`} style={{ display: Comment[item.id]?.display }}>
+              保存
+            </button>
+            <button onClick={() => handleDelete(item.id)} className={`comment_${item.id}`} style={{ display: Comment[item.id]?.display }}>
+              削除
+            </button>
+            <p>{item.commenter_user_name || item.commenter_company_name}</p>
+            <textarea
+              style={{
+                width: "50%",
+                height: "100px",
+              }}
+              value={Comment[item.id].text}
+              readOnly={Comment[item.id].readOnly} // 読み取り専用にする場合
+              onChange={(e) => handleChenge(e.target.value, item.id)}
+            />
+          </div>
+        ) : (
+          <div key={index}>
+            <hr />
+            <p>{item.commenter_user_name || item.commenter_company_name}</p>
+            <textarea
+              style={{
+                width: "50%",
+                height: "100px",
+              }}
+              value={item.content}
+              readOnly // 読み取り専用にする場合
+            />
+          </div>
+        )
+      )}
+    </>
+  );
+
   return (
     <>
       <Link to="/VideoList">動画一覧に戻る</Link>
@@ -124,6 +332,28 @@ const VideoDetailItem = () => {
         {renderYoutubeFrame}
         {renderGenre}
         {renderIntro}
+        <div>
+          <button onClick={handleTextOpen}>コメントする</button>
+          <br />
+          <div
+            style={{
+              display: CommentPost.display,
+            }}
+          >
+            <textarea
+              style={{
+                width: "50%",
+                height: "100px",
+              }}
+              value={CommentPost.text}
+              onChange={(e) => handlePostChange(e.target.value)}
+            />
+            <br />
+            <button onClick={() => handlePostCancel()}>キャンセル</button>
+            <button onClick={() => handlePost()}>投稿</button>
+          </div>
+        </div>
+        {renderComment}
       </Box>
       {/* 各項目の表示、ここまで */}
     </>

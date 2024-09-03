b8b7640a69e21c2ccb9af94ce5a0f0a485f0bc96
ryudayo64 <2211060@i-seifu.jp>
2024-09-03 18:05:16 +0900

    17:10

--- work_connect/frontend/src/sections/CompanyList/post-card.jsx
+++ work_connect/frontend/src/sections/CompanyList/post-card.jsx
@@ -8,14 +8,14 @@ import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 // import Stack from "@mui/material/Stack";
 import Avatar from "@mui/material/Avatar";
-// import { alpa } from "@mui/material/styles";
+import { follow } from "src/_mock/follow";
 import Grid from "@mui/material/Unstable_Grid2";
 import Typography from "@mui/material/Typography";
-import { follow } from "src/_mock/follow";
-import { fShortenNumber } from "src/utils/format-number";
-import Iconify from "src/components/iconify";
-import SvgColor from "src/components/svg-color";  
 
+import { useSessionStorage } from "src/hooks/use-sessionStorage";
+// import { fShortenNumber } from "src/utils/format-number";
+// import Iconify from "src/components/iconify";
+import SvgColor from "src/components/svg-color";
 // ----------------------------------------------------------------------
 
 const PostCard = forwardRef(({ post, index }, ref) => {
@@ -30,10 +30,8 @@ const PostCard = forwardRef(({ post, index }, ref) => {
   } = post;
 
   const [followStatus, setFollowStatus] = useState(initialFollowStatus);
-  // const navigate = useNavigate();
   const latestPostLarge = index === -1;
 
-  // const [followStatus, setFollowStatus] = useState(initialFollowStatus);
   const { getSessionData } = useSessionStorage();
   const accountData = getSessionData("accountData");
   const data = {
@@ -149,30 +147,6 @@ const PostCard = forwardRef(({ post, index }, ref) => {
       </div>
     ) : null;
 
-  const renderInfo = (
-    <Stack
-      direction="row"
-      flexWrap="wrap"
-      spacing={1.5}
-      justifyContent="flex-end"
-      sx={{
-        mt: 3,
-        color: "text.disabled",
-      }}
-    >
-      {[
-        { number: comment, icon: "eva:message-circle-fill" },
-        { number: view, icon: "eva:eye-fill" },
-        // { number: share, icon: "eva:share-fill" },
-      ].map((info, _index) => (
-        <Stack key={_index} direction="row">
-          <Iconify width={16} icon={info.icon} sx={{ mr: 0.5 }} />
-          <Typography variant="caption">{fShortenNumber(info.number)}</Typography>
-        </Stack>
-      ))}
-    </Stack>
-  );
-
   const renderCover = (
     <Box
       component="img"
@@ -246,8 +220,6 @@ const PostCard = forwardRef(({ post, index }, ref) => {
             {renderSelectedOccupation}
 
             {renderPrefecture}
-
-            {renderInfo}
           </Box>
         </Card>
       </div>

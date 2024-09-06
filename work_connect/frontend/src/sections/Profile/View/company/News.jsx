c209d7e882266992856755a83eea69255b4d13b5
Y2211029 <2211029@i-seifu.jp>
2024-09-06 15:03:17 +0900

    15:03

--- work_connect/frontend/src/sections/Profile/View/company/News.jsx
+++ work_connect/frontend/src/sections/Profile/View/company/News.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Helmet } from "react-helmet-async";
 
 import { SpecialCompanyNewsView } from "./SpecialCompanyNews";
@@ -17,23 +16,3 @@ export default function InternshipJobOfferPage() {
     </>
   );
 }
-=======
-import { Helmet } from "react-helmet-async";
-
-import { SpecialCompanyNewsView } from "./SpecialCompanyNews";
-
-
-// ----------------------------------------------------------------------
-
-export default function InternshipJobOfferPage() {
-  return (
-    <>
-      <Helmet>
-        <title> News | Minimal UI </title>
-      </Helmet>
-
-      <SpecialCompanyNewsView />
-    </>
-  );
-}
->>>>>>> 3c5789677e38c908589a20c4b753cb2d7d8e5230

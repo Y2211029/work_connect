3f04a7dd110d0d74fbe7d261e22e84d6ba524dce
bandou <2211023@i-seifu.jp>
2024-09-13 14:36:45 +0900

    14:36

--- work_connect/backend/app/Http/Controllers/tag/GetCompanySelectedOccupationTagController.php
+++ work_connect/backend/app/Http/Controllers/tag/GetCompanySelectedOccupationTagController.php
@@ -8,15 +8,26 @@ use Illuminate\Http\Request;
 
 class GetCompanySelectedOccupationTagController extends Controller
 {
-    public function GetCompanySelectedOccupationTagController(Request $request){
-        $tag = \DB::table('w_tags')
-            ->where('item_id', 21)
-            ->whereExists(function ($query) {
-                $query->select(\DB::raw(1))
-                    ->from('w_companies')
-                    ->whereRaw('w_companies.selected_occupation REGEXP CONCAT("(^|,)", w_tags.name, "(,|$)")');
-            })
-            ->get();
+    public function GetCompanySelectedOccupationTagController(Request $request)
+    {
+
+        if ($request->input("All", "") == "tags") {
+            $tag = \DB::table('w_tags')
+                ->where('item_id', 21)
+                ->get();
+        } else {
+            $tag = \DB::table('w_tags')
+                ->where('item_id', 21)
+                ->whereExists(function ($query) {
+                    $query->select(\DB::raw(1))
+                        ->from('w_companies')
+                        ->whereRaw('w_companies.selected_occupation REGEXP CONCAT("(^|,)", w_tags.name, "(,|$)")');
+                })
+                ->get();
+        }
+
+        \Log::info('GetCompanySelectedOccupationTagController.php:$tag:');
+        \Log::info(json_decode($tag, true));
         return json_encode($tag);
     }
 }

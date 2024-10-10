3a33813b8dd604f892507b4311e45be999ac0ab3
Y2211029 <2211029@i-seifu.jp>
2024-10-10 14:51:57 +0900

    14:51 | フォロー通知をクリックしてプロフィールに飛ぶ時の不具合を修正しました

--- work_connect/frontend/src/pages/Profile.jsx
+++ work_connect/frontend/src/pages/Profile.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 import axios from "axios";
 
 // インポート
@@ -12,7 +12,8 @@ export default function Profile() {
   const url = "http://localhost:8000/get_profile_mypage_kind";
 
   const { user_name } = useParams();
-  const [ ProfileUserName ] = useState(user_name);
+  const [ ProfileUserName, setProfileUserName ] = useState(user_name);
+  const location = useLocation();
 
   // 表示したいプロフィールの人のuser_nameを取得
   console.log("ProfileUserName:", ProfileUserName);
@@ -21,31 +22,38 @@ export default function Profile() {
   const [ResponseData, setResponseData] = useState([]);
 
   // ProfileUserNameが変化したとき
+  async function GetData(ProfileUserName) {
+
+    try {
+      // Laravel側からデータを取得
+      const response = await axios.get(url, {
+        params: {
+          ProfileUserName: ProfileUserName,
+        },
+      });
+      if(response.data){
+        setResponseData(response.data[0]);
+      } else {
+        // ユーザが見つからなかった場合
+        location.href = "/404";
+      }
+      console.log("response.data:", response.data);
+    } catch (err) {
+      console.log("err:", err);
+    }
+  }
+
   useEffect(() => {
     // 受け取ったuser_nameが学生か企業か判断する
-    async function GetData(ProfileUserName) {
-
-      try {
-        // Laravel側からデータを取得
-        const response = await axios.get(url, {
-          params: {
-            ProfileUserName: ProfileUserName,
-          },
-        });
-        if(response.data){
-          setResponseData(response.data[0]);
-        } else {
-          // ユーザが見つからなかった場合
-          location.href = "/404";
-        }
-        console.log("response.data:", response.data);
-      } catch (err) {
-        console.log("err:", err);
-      }
-    }
     // DBからデータを取得
     GetData(ProfileUserName);
-  }, [ProfileUserName]);
+    console.log("GetData!!!!!: ", ProfileUserName)
+  }, [ProfileUserName,  location.pathname, location.search]);
+
+  useEffect(() => {
+    setProfileUserName(user_name);
+  }, [user_name])
+
 
   if(ResponseData === "s"){
     // 学生側

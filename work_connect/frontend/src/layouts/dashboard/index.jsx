52e909e449cfbb0fb697b8271ae76308e39959c0
ryudayo64 <2211060@i-seifu.jp>
2024-10-24 15:17:33 +0900

    15:30

--- work_connect/frontend/src/layouts/dashboard/index.jsx
+++ work_connect/frontend/src/layouts/dashboard/index.jsx
@@ -72,25 +72,33 @@ export default function DashboardLayout({ children }) {
       const newWs = new WebSocket(`ws://localhost:3000?userId=${accountData.id}`);
       newWs.onmessage = (event) => {
         const data = JSON.parse(event.data);
+        console.log("index.js : data:", data);
         if (data.kind === "notification") {
           if (data.type === "follow") {
-            console.log("通ってると", data.noticeData);
+            console.log("index.js : data:", data);
             setWebSocketState((prev) => ({
               ...prev,
               notification: data,
               websocketFollowStatus: data.followData,
             }));
+          } else if(data.type === "videoPosting") {
+            console.log("index.js : data:", data);
+            setWebSocketState((prev) => ({
+              ...prev,
+              notification: data,
+            }));
           }
         }
         if (data.kind === "follow") {
           if (data.type === "follow") {
-            console.log("通ってると", data);
+            console.log("index.js : data.noticeData :", data);
             setWebSocketState((prev) => ({
               ...prev,
               websocketFollowStatus: data,
             }));
           }
         }
+    
       };
       newWs.onclose = () => {
         console.log("WebSocket connection closed");

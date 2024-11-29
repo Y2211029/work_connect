fa02b8b2a33c3d395f4c21aac713085aed95852a
Y2211029 <2211029@i-seifu.jp>
2024-11-29 15:28:53 +0900

    15:28｜プロフィール画面での学校名一覧をJSONファイルから取得するようにしました

--- work_connect/frontend/src/sections/Profile/View/student/EditRequiredFields/SchoolName.jsx
+++ work_connect/frontend/src/sections/Profile/View/student/EditRequiredFields/SchoolName.jsx
@@ -1,57 +1,58 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
+// import axios from "axios";
 import Select from "react-select";
 import { useSessionStorage } from "src/hooks/use-sessionStorage";
 import PropTypes from 'prop-types';
+import schoolList from 'src/data/school_list.json';
 
-const schoolTypeCodes = ["H1", "F1"]; // 複数のschool_type_codeを配列として定義
-const accessToken = "268|G5fHGAGA7Col8FetXAQ6EMNHnjDIA5TInN2uByIB";
-const options = []; // ここは初期値を空の配列に
+// const schoolTypeCodes = ["H1", "F1"]; // 複数のschool_type_codeを配列として定義
+// const accessToken = "268|G5fHGAGA7Col8FetXAQ6EMNHnjDIA5TInN2uByIB";
+const options = schoolList;
 
 ////////////////////////////////////////
 // 学校情報を取得
 const fetchSchoolData = async () => {
-  let allSchools = [];
-  try {
-    for (const code of schoolTypeCodes) {
-      let page = 1;
-      let hasMore = true;
+  // let allSchools = [];
+  // try {
+  //   for (const code of schoolTypeCodes) {
+  //     let page = 1;
+  //     let hasMore = true;
 
-      while (hasMore) {
-        const response = await axios.get(
-          // `https://api.edu-data.jp/api/v1/school?school_type_code=${code}&pref_code=27&page=${page}&school_status_code=1,2`,
-          `https://api.edu-data.jp/api/v1/school?school_type_code=${code}&page=${page}&school_status_code=1,2`,
-          {
-            headers: {
-              Authorization: `Bearer ${accessToken}`,
-              Accept: "application/json",
-            },
-          }
-        );
+  //     while (hasMore) {
+  //       const response = await axios.get(
+  //         // `https://api.edu-data.jp/api/v1/school?school_type_code=${code}&pref_code=27&page=${page}&school_status_code=1,2`,
+  //         `https://api.edu-data.jp/api/v1/school?school_type_code=${code}&page=${page}&school_status_code=1,2`,
+  //         {
+  //           headers: {
+  //             Authorization: `Bearer ${accessToken}`,
+  //             Accept: "application/json",
+  //           },
+  //         }
+  //       );
 
-        const data = response.data.schools.data || [];
-        if (Array.isArray(data)) {
-          options.push(
-            ...data
-              .filter(school => school.school_name) // 空でない学校名をフィルタリング
-              .map(school => ({
-                value: school.school_name,
-                label: school.school_name
-              }))
-          );
+  //       const data = response.data.schools.data || [];
+  //       if (Array.isArray(data)) {
+  //         options.push(
+  //           ...data
+  //             .filter(school => school.school_name) // 空でない学校名をフィルタリング
+  //             .map(school => ({
+  //               value: school.school_name,
+  //               label: school.school_name
+  //             }))
+  //         );
 
-          allSchools = [...allSchools, ...data];
-          hasMore = data.length > 0;
-          page += 1;
-        } else {
-          console.error("Unexpected response format:", response.data);
-          hasMore = false;
-        }
-      }
-    }
-  } catch (error) {
-    console.error("Error fetching data:", error);
-  }
+  //         allSchools = [...allSchools, ...data];
+  //         hasMore = data.length > 0;
+  //         page += 1;
+  //       } else {
+  //         console.error("Unexpected response format:", response.data);
+  //         hasMore = false;
+  //       }
+  //     }
+  //   }
+  // } catch (error) {
+  //   console.error("Error fetching data:", error);
+  // }
 };
 
 fetchSchoolData();
